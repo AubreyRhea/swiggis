15a0177ce8b4eaff66236f904721733513d47ca9
Jeremiah Johnson <jjohnson@saminc.biz>
2014-04-29 08:40:22 -0500

    Map works. No attributes yet.

--- introToJS/javascript.js
+++ introToJS/javascript.js
@@ -9,6 +9,55 @@ var imageTitles = ["#13 SB I-35 Upper Deck", "#18 Zilker Clubhouse", "#28 N Cong
 var imageLocations = [[-97.727314, 30.283061], [-97.779239, 30.270478], [-97.737999, 30.280725], [-97.717332, 30.243198],
     [-97.742814, 30.268052], [-97.707052, 30.258991], [-97.699774, 30.292478], [-97.723506, 30.295442], [-97.727969, 30.283250]];
 
+var images = {
+    0: {
+        title: "#13 SB I-35 Upper Deck",
+        path: "Images/Capitol 009.JPG",
+        location: [-97.727314, 30.283061]
+    },
+    1: {
+        title: "#18 Zilker Clubhouse",
+        path: "Images/Capitol 070.JPG",
+        location: [-97.779239, 30.270478]
+    },
+    2: {
+        title: "#28 N Congress @ MLK Blvd",
+        path: "Images/Capitol 104.JPG",
+        location: [-97.737999, 30.280725]
+    },
+    3: {
+        title: "#10 Pleasant Valley @ S Lakeshore",
+        path: "Images/Lakeshore 009.JPG",
+        location: [-97.717332, 30.243198],
+    },
+    4: {
+        title: "#6 Congress @ 6th St",
+        path: "Images/Lemon 006.JPG",
+        location: [-97.742814, 30.268052]
+    },
+    5: {
+        title: "#16 E 7th Street Bridge over Texas-New Orleans RR",
+        path: "Images/MLK 021.JPG",
+        location: [-97.707052, 30.258991]
+    },
+    6: {
+        title: "#23 Robert Mueller Airport",
+        path: "Images/Mueller 008.JPG",
+        location: [-97.699774, 30.292478],
+    },
+    7: {
+        title: "#22 38th St @ Red River",
+        path: "Images/Noodle 016.JPG",
+        location: [-97.723506, 30.295442]
+    },
+    8: {
+        title: "#27 LBJ Library",
+        path: "Images/Noodle 025.JPG",
+        location: [-97.727969, 30.283250]
+    }
+}
+
+
 
 //on(map, "load", PageLoad());
 //PageLoad();
@@ -85,23 +134,32 @@ function EnableNavigationButtons() {
 }
 
 require([
-    "esri/map", "esri/symbols/SimpleMarkerSymbol", "esri/graphic", "esri/geometry/Point", "dojo/domReady!"
+    "dojo/on", "dojo/dom",
+    "esri/map", "esri/symbols/SimpleMarkerSymbol", "esri/symbols/SimpleLineSymbol", "esri/graphic", "esri/geometry/Point", "esri/graphicsUtils", "esri/Color",
+    "dojo/domReady!"
 ], function (
-    Map, SimpleMarkerSymbol, Graphic, Point
+    on, dom,
+    Map, SimpleMarkerSymbol, SimpleLineSymbol, Graphic, Point, GraphicsUtils, Color
     ) {
     var map = new Map("mapDiv", {
-        center: [-98.5795, 39.8282],
-        zoom: 3,
+        center: [-97.742581, 30.2837352],
+        zoom: 12,
         basemap: "streets"
     });
 
-    var symbol = new SimpleMarkerSymbol();
+    var symbol = new SimpleMarkerSymbol(SimpleMarkerSymbol.STYLE_CIRCLE, 15,
+        new SimpleLineSymbol(SimpleLineSymbol.STYLE_SOLID,
+        new Color("blue"), 1),
+        new Color("teal"));
+    //symbol.setColor = new Color([100, 100, 100]);
 
-    for (var i in imageLocations) {
-        var point = new Point(imageLocations[i]);
-        console.log(point);
-        var graphic = new Graphic(point, symbol);
-        console.log(graphic);
-        map.graphics.add(graphic)
+    var load = dom.byId("load");
+    on(map, "load", loadGraphics);
+    function loadGraphics() {
+        for (var i in imageLocations) {
+            var point = new Point(imageLocations[i]);
+            var graphic = new Graphic(point, symbol);
+            map.graphics.add(graphic)
+        }
     }
 });
